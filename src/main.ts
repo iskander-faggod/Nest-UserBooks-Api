920cf8e4782478ab72a0d54fcc02619bdb775db8
Iskander <grinerplaydark@gmail.com>
2021-06-17 16:25:45 +0300

    error with services

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { config } from 'rxjs';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   await app.listen(3000);
+  const options = new DocumentBuilder()
+    .setTitle('User-Book API')
+    .setDescription('The test task')
+    .setVersion('1.0')
+    .addTag('shopApi')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('swagger', app, document);
 }
 bootstrap().then(r => console.log('Connected'));
--- src/modules/User/user.module.ts
+++ src/modules/User/user.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Book } from '../Book /entities/book.entity';
 import { User } from './entities/user.entity';
+import { BookController } from './controllers/user.controller';
+import { BooksService } from '../Book /services/book.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User, Book])],
+  imports: [TypeOrmModule.forFeature([User, Book]), BooksService],
   controllers: [],
   providers: [],
   exports : [TypeOrmModule]
