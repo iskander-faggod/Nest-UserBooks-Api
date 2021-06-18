08d3856e107c95cf545144dafdc72df22fc19618
Iskander <grinerplaydark@gmail.com>
2021-06-18 20:46:13 +0300

    all httpexception

--- src/modules/Book /controllers/book.controller.ts
+++ src/modules/Book /controllers/book.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpException, Param, Post, Put } from '@nestjs/common';
 import { Book } from '../entities/book.entity';
 import { CreateBookDto, EditBookDto, ReturnBookDto } from '../dto/book.dto';
 import { BooksService } from '../services/book.service';
@@ -15,19 +15,19 @@ export class BookController {
   constructor(private readonly bookService: BooksService) {
   }
 
-  @ApiOperation({ summary: "Получение списка всех книг" })
+  @ApiOperation({ summary: 'Получение списка всех книг' })
   @Get()
   getAllBooks(): Promise<Book[]> {
     return this.bookService.findAllBooks();
   }
 
-  @ApiOperation({ summary: "Получение книги по id" })
+  @ApiOperation({ summary: 'Получение книги по id' })
   @Get(':id')
   getOneBook(@Param('id') bookId: string): Promise<Book> {
     return this.bookService.findOneBook(bookId);
   }
 
-  @ApiOperation({ summary: "Создание новой книги" })
+  @ApiOperation({ summary: 'Создание новой книги' })
   @Post()
   createBook(@Body() createBookDto: CreateBookDto): Promise<Book> {
     const book = new Book();
@@ -39,14 +39,12 @@ export class BookController {
     return this.bookService.createBook(book);
   }
 
-  @ApiOperation({ summary: "Редактировать книгу" })
+  @ApiOperation({ summary: 'Редактировать книгу' })
   @Put(':id')
   async editBook(@Body() editBookDto: EditBookDto, @Param('id') id: string): Promise<Book | { error }> {
     const book = await this.bookService.findOneBook(id);
     if (book == undefined) {
-      return {
-        error: 'Book not found',
-      };
+      throw  new HttpException('Cannot edit book', 404);
     }
     book.authorName = editBookDto.authorName;
     book.booksName = editBookDto.booksName;
@@ -54,23 +52,26 @@ export class BookController {
     return this.bookService.editBook(book);
   }
 
-  @ApiOperation({ summary: "Возврат книги пользователю" })
+  @ApiOperation({ summary: 'Возврат книги пользователю' })
   @Put('return/:id')
-  async returnBook(@Body() returnBookDto: ReturnBookDto, @Param('id') id: string): Promise<Book>{
+  async returnBook(@Body() returnBookDto: ReturnBookDto, @Param('id') id: string): Promise<Book> {
     const book = await this.bookService.findOneBook(id);
+    if (book == undefined) {
+      throw  new HttpException('Cannot return book', 404);
+    }
     book.isActive = false;
-    return this.bookService.returnBook(book)
+    return this.bookService.returnBook(book);
   }
 
-  @ApiOperation({ summary: "Удаление книги" })
+  @ApiOperation({ summary: 'Удаление книги' })
   @Delete(':id')
   deleteBook(@Param('id') bookId: string): Promise<void> {
     return this.bookService.removeBook(bookId);
   }
 
-  @ApiOperation({ summary: "Получение книги по автору и названию" })
+  @ApiOperation({ summary: 'Получение книги по автору и названию' })
   @Get(':author:title')
-  getByValue(@Param("author") author: string, @Param("title") title: string) {
+  getByValue(@Param('author') author: string, @Param('title') title: string) {
     return this.bookService.getBookByValue(author, title);
   }
 
